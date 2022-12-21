ba8728a517125fa515e259e5d1f28302a3a1d579
youssef tadlaoui <usftadlaoui@gmail.com>
2022-12-21 14:58:26 +0100

    data

---
+++ script/scripte.js
@@ -0,0 +1,6 @@
+var valueRadio = null; //stor value
+const btnStart = document.querySelector('#btnStart');
+btnStart.addEventListener('click',quizStart)
+function quizStart(){
+    console.log("hello");
+}
