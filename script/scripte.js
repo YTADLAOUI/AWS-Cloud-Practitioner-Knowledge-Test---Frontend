7a67f259db3fbac9e8b0973c61986c96baa09180
youssef tadlaoui <usftadlaoui@gmail.com>
2022-12-22 15:37:32 +0100

    quiz

--- script/scripte.js
+++ script/scripte.js
@@ -1,28 +1,38 @@
 var valueRadio = null; //stor value
 const btnStart = document.querySelector('#btnStart');
-btnStart.addEventListener('click',quizStart)
+btnStart.addEventListener('click',quiz)
 function countDown(){
     for (let i = 5; i >= 0; i--) {
         setTimeout(() =>{
             document.getElementById("counter-to-start").innerText = `${i}`
             if(i===0){
-                document.getElementById("counter-to-start").innerText = `let's Go`
+                document.getElementById("counter-to-start").innerText = ``
+                document.getElementById("quz").innerText = `` 
                 quizStart()
             }
         }, (5 - i) * 1000);
     }
 }
 
-function quizStart(){
+function quiz(){
     var fullname=document.getElementById("Fullname").value;
     if(fullname===""){
         document.getElementById("alert").innerText= `s'il vous plait Entre votre name`;
     }else{
         document.querySelector(".lance").innerHTML=`
-        <article>
-        <h1>Quiz well start in <br><span id="counter-to-start"></span></h1>
+        <article id="quz">
+        <h1 class="quiz">Quiz well start in <br><span id="counter-to-start"></span></h1>
         </article>`
         countDown();
-    }
-        
+    }  
       }
+
+    function quizStart(){
+        
+        document.getElementById("quz").innerText = `
+        
+        
+        
+        ` 
+
+    }
