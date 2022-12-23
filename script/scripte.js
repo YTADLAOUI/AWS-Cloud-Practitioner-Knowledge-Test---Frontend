9ff4c6581a0c2d0d2911044f905c6a495f259d9f
youssef tadlaoui <usftadlaoui@gmail.com>
2022-12-23 15:01:22 +0100

    recupere

--- script/scripte.js
+++ script/scripte.js
@@ -1,6 +1,9 @@
 var valueRadio = null; //stor value
 const btnStart = document.querySelector('#btnStart');
-const btnNext = document.getElementById('#next');
+const qu1 = document.getElementById('#next');
+const qu2 = document.getElementById('#qu2');
+const qu3 = document.getElementById('#qu3');
+const qu4 = document.getElementById('#qu4');
 
 // console.log(btnNext)
 
@@ -33,9 +36,7 @@ function quiz(){
     }  
 }
 
-// let y=0;
-// let x=0;
-// let i=0;
+
 function quizStart(id){
         
         
@@ -46,21 +47,21 @@ function quizStart(id){
                 <progress id="prog" value=${id+1} max="10">3</progress>
                 </div>
                 <p id="title">Q${id+1}: ${question[id].title}</p>
-            <form class="px" id="form">
+            <form class="" id="form">
             <div><input type="radio" id="qu1" name="fav_language" value="ql">
             <label for="q1">${question[id].options[0]}</label><br></div>
-            
-            <input type="radio" id="qu1" name="fav_language" value="ql">
+            <input type="radio" id="qu2" name="fav_language" value="ql">
             <label for="q1">${question[id].options[1]}</label><br>
-            <input type="radio" id="qu1" name="fav_language" value="ql">
+            <input type="radio" id="qu3" name="fav_language" value="ql">
             <label for="q1">${question[id].options[2]}</label><br>
-            <input type="radio" id="qu1" name="fav_language" value="ql">
+            <input type="radio" id="qu4" name="fav_language" value="ql">
             <label for="q1">${question[id].options[3]}</label><br>
             
             </form>
             <div class="btns" id="switchBtns">
                 ${id==0 ? "" :`<button id="previous" name="button" onclick=quizStart(${id-1}) class="inactive">Précédent</button>`}
                 <button id="next" onclick=quizStart(${id+1}) name="button">Suivante</button>
+                ${id==9 ? `<button id="previous" name="button" onclick= class="inactive">Résultat</button>` :""}
             </div>
         </div>
         ` ;
