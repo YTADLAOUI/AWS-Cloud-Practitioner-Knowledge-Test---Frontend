366abe60ac58b4d985f94c13fe8863fa7f2d31ee
Youssef tdl <usftadlaoui@gmail.com>
2022-12-25 01:52:23 +0100

    js termin

--- script/data.js
+++ script/data.js
@@ -1,62 +1,61 @@
-const question =[
-    {
-    title: 'Why is AWS more economical than traditional data centers for applications with varying computeworkloads ?',
-    type: 'radio',
-    options: ['Amazon EC2 costs are billed on a monthly basis', 'Users retain full administrative access to their Amazon EC2 instances','Amazon EC2 instances can be launched on demand when needed.','Users can permanently run enough instances to handle peak workloads.'],
-    answer: ''
+const question = [{
+        title: 'Why is AWS more economical than traditional data centers for applications with varying computeworkloads ?',
+        type: 'radio',
+        options: ['Amazon EC2 costs are billed on a monthly basis', 'Users retain full administrative access to their Amazon EC2 instances', 'Amazon EC2 instances can be launched on demand when needed.', 'Users can permanently run enough instances to handle peak workloads.'],
+        answer: 'c'
     },
     {
-    title: 'Which AWS service would simplify the migration of a database to AWS?',
-    type: 'radio',
-    options: ['AWS Storage Gateway','AWS Database Migration Service (AWS DMS)','Amazon EC2','Amazon AppStream 2.0'],
-    answer: ''
+        title: 'Which AWS service would simplify the migration of a database to AWS?',
+        type: 'radio',
+        options: ['AWS Storage Gateway', 'AWS Database Migration Service (AWS DMS)', 'Amazon EC2', 'Amazon AppStream 2.0'],
+        answer: 'b'
     },
     {
-    title: 'Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?',
-    type: 'radio',
-    options: ['AWS Config','AWS OpsWorks','AWS SDK','AWS Marketplace'],
-    answer: ''
+        title: 'Which AWS offering enables users to find, buy, and immediately start using software solutions in their AWS environment?',
+        type: 'radio',
+        options: ['AWS Config', 'AWS OpsWorks', 'AWS SDK', 'AWS Marketplace'],
+        answer: 'd'
     },
     {
-    title: 'Which AWS networking service enables a company to create a virtual network within AWS?',
-    type: 'radio',
-    options: ['AWS Config','Amazon Route 53','AWS Direct Connect','Amazon Virtual Private Cloud (Amazon VPC)'],
-    answer: ''
+        title: 'Which AWS networking service enables a company to create a virtual network within AWS?',
+        type: 'radio',
+        options: ['AWS Config', 'Amazon Route 53', 'AWS Direct Connect', 'Amazon Virtual Private Cloud (Amazon VPC)'],
+        answer: 'd'
     },
     {
-    title: ' Which of the following is an AWS responsibility under the AWS shared responsibility model?',
-    type: 'radio',
-    options: ['Configuring third-party applications','Maintaining physical hardware ','Securing application access and data','Managing guest operating systems'],
-    answer: ''
+        title: ' Which of the following is an AWS responsibility under the AWS shared responsibility model?',
+        type: 'radio',
+        options: ['Configuring third-party applications', 'Maintaining physical hardware ', 'Securing application access and data', 'Managing guest operating systems'],
+        answer: 'b'
     },
     {
-    title: 'Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?',
-    type: 'radio',
-    options: ['AWS Regions','Edge locations','Availability Zones','Virtual Private Cloud (VPC)'],
-    answer: ''
+        title: 'Which component of the AWS global infrastructure does Amazon CloudFront use to ensure low-latency delivery?',
+        type: 'radio',
+        options: ['AWS Regions', 'Edge locations', 'Availability Zones', 'Virtual Private Cloud (VPC)'],
+        answer: 'b'
     },
     {
-    title: 'How would a system administrator add an additional layer of login security to a user\'s AWSManagement Console?',
-    type: 'radio',
-    options: ['Use Amazon Cloud Directory','Audit AWS Identity and Access Management (IAM) roles','Enable multi-factor authentication','Enable AWS CloudTrail'],
-    answer: ''
+        title: 'How would a system administrator add an additional layer of login security to a user\'s AWSManagement Console?',
+        type: 'radio',
+        options: ['Use Amazon Cloud Directory', 'Audit AWS Identity and Access Management (IAM) roles', 'Enable multi-factor authentication', 'Enable AWS CloudTrail'],
+        answer: 'c'
     },
     {
-    title: 'Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?',
-    type: 'radio',
-    options: ['AWS Trusted Advisor','AWS CloudTrail','AWS X-Ray','AWS Identity and Access Management (AWS IAM)'],
-    answer: ''
+        title: 'Which service can identify the user that made the API call when an Amazon EC2 instance is terminated?',
+        type: 'radio',
+        options: ['AWS Trusted Advisor', 'AWS CloudTrail', 'AWS X-Ray', 'AWS Identity and Access Management (AWS IAM)'],
+        answer: 'b'
     },
     {
-    title: 'Which service would be used to send alerts based on Amazon CloudWatch alarms?',
-    type: 'radio',
-    options: ['Amazon Simple Notification Service (Amazon SNS)','AWS CloudTrail','AWS Trusted Advisor','Amazon Route 53'],
-    answer: ''
+        title: 'Which service would be used to send alerts based on Amazon CloudWatch alarms?',
+        type: 'radio',
+        options: ['Amazon Simple Notification Service (Amazon SNS)', 'AWS CloudTrail', 'AWS Trusted Advisor', 'Amazon Route 53'],
+        answer: 'a'
     },
     {
-    title: 'Where can a user find information about prohibited actions on the AWS infrastructure?',
-    type: 'radio',
-    options: ['AWS Trusted Advisor','AWS Identity and Access Management (IAM)','AWS Billing Console','AWS Acceptable Use Policy'],
-    answer: ''
+        title: 'Where can a user find information about prohibited actions on the AWS infrastructure?',
+        type: 'radio',
+        options: ['AWS Trusted Advisor', 'AWS Identity and Access Management (IAM)', 'AWS Billing Console', 'AWS Acceptable Use Policy'],
+        answer: 'd'
     },
 ]
--- script/scripte.js
+++ script/scripte.js
@@ -1,46 +1,46 @@
-var valueRadio = null; //stor value
-const btnStart = document.querySelector('#btnStart');
-const qu1 = document.getElementById('#next');
-const qu2 = document.getElementById('#qu2');
-const qu3 = document.getElementById('#qu3');
-const qu4 = document.getElementById('#qu4');
+ var valueRadio = null; //stor value
+ var Name = null;
+ const btnStart = document.querySelector('#btnStart');
+ // console.log(btnNext)
+ btnStart.addEventListener('click', quiz);
+ // btnNext.addEventListener('click',quizStart);
 
-// console.log(btnNext)
+ let correct = 0;
+ let echac = 0;
 
-btnStart.addEventListener('click',quiz);
-// btnNext.addEventListener('click',quizStart);
-function countDown(){
-    for (let i = 1; i >= 0; i--) {
-        setTimeout(() =>{
-            document.getElementById("counter-to-start").innerText = `${i}`
-            if(i===0){
-                document.getElementById("counter-to-start").innerText = ``
-                document.getElementById("quz").innerText = `` 
-
-                quizStart(0)
-            }
-        }, (1 - i) * 1000);
-    }
-}
-function quiz(){
-    var fullname=document.getElementById("Fullname").value;
-    if(fullname===""){
-        document.getElementById("alert").innerText= `s'il vous plait Entre votre name`;
-    }else{
-        document.querySelector(".lance").innerHTML=`
+ function quiz() {
+     var fullname = document.getElementById("Fullname").value;
+     Name = fullname;
+     console.log(Name)
+     if (fullname === "") {
+         document.getElementById("alert").innerText = `s'il vous plait Entre votre name`;
+     } else {
+         document.querySelector(".lance").innerHTML = `
         <article id="quz">
         <h1 class="quiz">Quiz well start in <br><span id="counter-to-start"></span></h1>
         </article>`
-        countDown();
+         countDown();
+     }
+ }
 
-    }  
-}
 
+ function countDown() {
+     for (let i = 1; i >= 0; i--) {
+         setTimeout(() => {
+             document.getElementById("counter-to-start").innerText = `${i}`
+             if (i === 0) {
+                 document.getElementById("counter-to-start").innerText = ``
+                 document.getElementById("quz").innerText = ``
 
-function quizStart(id){
-        
-        
-        document.getElementById("quz").innerHTML = `
+                 quizStart(0);
+             }
+         }, (1 - i) * 1000);
+     }
+ }
+
+ function quizStart(id) {
+
+     document.getElementById("quz").innerHTML = `
         <div id ="" class="" >
             <div id="fil" class="">
                 <div class=""><span id='counter'>${id+1}</span>/10</div>
@@ -48,26 +48,90 @@ function quizStart(id){
                 </div>
                 <p id="title">Q${id+1}: ${question[id].title}</p>
             <form class="" id="form">
-            <div><input type="radio" id="qu1" name="fav_language" value="ql">
-            <label for="q1">${question[id].options[0]}</label><br></div>
-            <input type="radio" id="qu2" name="fav_language" value="ql">
-            <label for="q1">${question[id].options[1]}</label><br>
-            <input type="radio" id="qu3" name="fav_language" value="ql">
-            <label for="q1">${question[id].options[2]}</label><br>
-            <input type="radio" id="qu4" name="fav_language" value="ql">
-            <label for="q1">${question[id].options[3]}</label><br>
+            <div><input type="radio" class="qs" id="a"  name="fav"  >
+            <label for="a">${question[id].options[0]}</label><br></div>
+            <input type="radio" class="qs" id="b"  name="fav"  >
+            <label for="b">${question[id].options[1]}</label><br>
+            <input type="radio" class="qs" id="c"  name="fav"  >
+            <label for="c">${question[id].options[2]}</label><br>
+            <input type="radio" class="qs" id="d"  name="fav"  >
+            <label for="d">${question[id].options[3]}</label><br>
             
             </form>
             <div class="btns" id="switchBtns">
-                ${id==0 ? "" :`<button id="previous" name="button" onclick=quizStart(${id-1}) class="inactive">Précédent</button>`}
-                <button id="next" onclick=quizStart(${id+1}) name="button">Suivante</button>
-                ${id==9 ? `<button id="previous" name="button" onclick= class="inactive">Résultat</button>` :""}
+               ${id==9 ? `<button id="previous" name="button" onclick=recuper(${id});resultat(); quizStart(${id+1}) class="inactive">Résultat</button>` :`<button id="next" onclick=recuper(${id});quizStart(${id+1}) name="button">Suivante</button>`} 
+                
             </div>
         </div>
-        ` ;
-        // x++;
-        
-    
-    
-console.log(id);
+        `;
 }
+
+
+
+function recuper(id) {
+   
+    const destts = document.querySelectorAll('.qs');
+    destts.forEach(destt => {
+            //    console.log(destt.checked)
+            if (destt.checked) {
+                // console.log(destt.id == question[id].answer, "comparision")
+                // console.log(destt.id, "user")
+                // console.log(question[id].answer, "data")
+                valueRadio=destt.id ;
+            }
+        }
+        )
+        if (valueRadio == question[id].answer) {
+            //console.log("yes")
+            correct++;
+            
+        } else {
+            //console.log("errou")
+            echac++;
+        }
+         console.log(correct)
+         console.log(echac)
+        
+} 
+function resultat(){
+    document.getElementById("quz").innerHTML = 
+    `<div>
+    <div>${Name}</div>
+    <div><p>les réponses correcte est ${correct}</p></div>
+    <div>les réponses fausse ${echac}</div>
+    ${correct>=9 ? `<div><p>mention : Tres bien</p> </div>`:correct>=7 ? `<div><p>mention :  bien</p> </div>`:correct==6?`<div><p>mention : Assez bien</p> </div>`:correct==5?`<div><p>mention : Passable</p> </div>`:`<div><p>mention : rattrapage</p> </div>`}
+    <button id="previous" name="button" onclick=answer() class="inactive">Answers</button>
+    </div>`;
+    
+}
+function answer(){document.getElementById("quz").innerHTML =  `<div><pre>Answers
+1) C – The ability to launch instances on demand when needed allows users to launch and terminate instances in
+response to a varying workload. This is a more economical practice than purchasing enough on-premises servers
+to handle the peak load.
+2) B – AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains
+fully operational during the migration, minimizing downtime to applications that rely on the database. AWS DMS
+can migrate data to and from most widely used commercial and open-source databases.
+3) D – AWS Marketplace is a digital catalog with thousands of software listings from independent software
+vendors that makes it easy to find, test, buy, and deploy software that runs on AWS.
+4) D – Amazon VPC lets users provision a logically isolated section of the AWS Cloud where users can launch
+AWS resources in a virtual network that they define.
+5) B – Maintaining physical hardware is an AWS responsibility under the AWS shared responsibility model.
+6) B – To deliver content to users with lower latency, Amazon CloudFront uses a global network of points of
+presence (edge locations and regional edge caches) worldwide.
+7) C – Multi-factor authentication (MFA) is a simple best practice that adds an extra layer of protection on top of a
+username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be
+prompted for their username and password (the first factor—what they know), as well as for an authentication
+code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide
+increased security for AWS account settings and resources.
+8) B – AWS CloudTrail helps users enable governance, compliance, and operational and risk auditing of their
+AWS accounts. Actions taken by a user, role, or an AWS service are recorded as events in CloudTrail. Events
+include actions taken in the AWS Management Console, AWS Command Line Interface (CLI), and AWS SDKs
+and APIs.
+9) A – Amazon SNS and Amazon CloudWatch are integrated so users can collect, view, and analyze metrics for
+every active SNS. Once users have configured CloudWatch for Amazon SNS, they can gain better insight into the
+performance of their Amazon SNS topics, push notifications, and SMS deliveries.
+10) D – The AWS Acceptable Use Policy provides information regarding prohibited actions on the AWS
+infrastructure. </pre>
+<button id="previous" name="button" onclick=resultat() class="inactive">Resultat</button>
+</div>
+`}
