58ed8eb7e27e5b7db7b534ccdf93ffbeb49b0e73
youssef tadlaoui <usftadlaoui@gmail.com>
2022-12-27 11:48:27 +0100

    answers fausses

--- script/scripte.js
+++ script/scripte.js
@@ -1,14 +1,14 @@
 question.sort(function(){ return Math.random() - 0.5;});
- var valueRadio = null; //stor value
- var Name = null;
- var numarr;
+ var valueRadio; //stor value
+ var Name;
+ var num;
+ //var repenseQ;
+//  var numarr;
  let array=[];
  const btnStart = document.querySelector('#btnStart');
  let step2 = document.querySelector(".step-2");
  let step1 = document.querySelector(".step-1");
  let step3 = document.querySelector(".step-3");
-
-
  // console.log(btnNext)
  btnStart.addEventListener('click', quiz);
  // btnNext.addEventListener('click',quizStart);
@@ -34,7 +34,6 @@ question.sort(function(){ return Math.random() - 0.5;});
         step1.classList.remove('circle-acitve');
      }
  }
-
 //  function random(){
 //     if(conteur<=9){
 //      do {
@@ -53,7 +52,6 @@ question.sort(function(){ return Math.random() - 0.5;});
 //          console.log(numarr)
 //     }
 //  }
-
  function countDown() {
      for (let i = 5; i >= 0; i--) {
          setTimeout(() => {
@@ -114,55 +112,41 @@ function recuper(id) {
             correct++;
             
         } else {
+            repenseQ=question[id].repense;
+            console.log(repenseQ);
+            array.push(repenseQ);
             //console.log("errou")
             echac++;
         }
          //console.log(correct)
-         //console.log(echac)
-        
+         //console.log(echac) 
 } 
 function resultat(){
     document.getElementById("quz").innerHTML = 
     `<div class="resultat">
     <div class="name">${Name}</div>
-    <div class= "correct"><p>Les réponses correcte est ${correct}</p></div>
-    <div class="fausse">Les réponses fausse est ${echac}</div>
+    <div class= "correct"><p>Les réponses correctes est ${correct}</p></div>
+    <div class="fausse">Les réponses fausses est ${echac}</div>
     ${correct>=9 ? `<div class="mention Tres"><p>mention : Tres bien</p> </div>`:correct>=7 ? `<div class="mention bien"><p>mention :  bien</p> </div>`:correct==6?`<div class="mention assez"><p>mention : Assez bien</p> </div>`:correct==5?`<div class="mention passable"><p>mention : Passable</p> </div>`:`<div class="mention danger"><p>mention : rattrapage</p> </div>`}
     <div class="btnansr"><button id="ans"  name="button" onclick=answer()>Answers</button></div>
     </div>`;
-
         step3.classList.add('circle-acitve');
         step2.classList.remove('circle-acitve');
 }
-function answer(){document.getElementById("quz").innerHTML =  `<div class= "Answers"><pre>
-Answers:
-1) C – The ability to launch instances on demand when needed allows users to launch and terminate instances in
-response to a varying workload. This is a more economical practice than purchasing enough on-premises servers
-to handle the peak load.
-2) B – AWS DMS helps users migrate databases to AWS quickly and securely. The source database remains
-fully operational during the migration, minimizing downtime to applications that rely on the database. AWS DMS
-can migrate data to and from most widely used commercial and open-source databases.
-3) D – AWS Marketplace is a digital catalog with thousands of software listings from independent software
-vendors that makes it easy to find, test, buy, and deploy software that runs on AWS.
-4) D – Amazon VPC lets users provision a logically isolated section of the AWS Cloud where users can launch
-AWS resources in a virtual network that they define.
-5) B – Maintaining physical hardware is an AWS responsibility under the AWS shared responsibility model.
-6) B – To deliver content to users with lower latency, Amazon CloudFront uses a global network of points of
-presence (edge locations and regional edge caches) worldwide.
-7) C – Multi-factor authentication (MFA) is a simple best practice that adds an extra layer of protection on top of a
-username and password. With MFA enabled, when a user signs in to an AWS Management Console, they will be
-prompted for their username and password (the first factor—what they know), as well as for an authentication
-code from their MFA device (the second factor—what they have). Taken together, these multiple factors provide
-increased security for AWS account settings and resources.
-8) B – AWS CloudTrail helps users enable governance, compliance, and operational and risk auditing of their
-AWS accounts. Actions taken by a user, role, or an AWS service are recorded as events in CloudTrail. Events
-include actions taken in the AWS Management Console, AWS Command Line Interface (CLI), and AWS SDKs
-and APIs.
-9) A – Amazon SNS and Amazon CloudWatch are integrated so users can collect, view, and analyze metrics for
-every active SNS. Once users have configured CloudWatch for Amazon SNS, they can gain better insight into the
-performance of their Amazon SNS topics, push notifications, and SMS deliveries.
-10) D – The AWS Acceptable Use Policy provides information regarding prohibited actions on the AWS
-infrastructure. </pre>
-<button id="previous" name="button" onclick=resultat() class="inactive">Resultat</button>
+function answer(){
+    document.getElementById("quz").innerHTML =  `
+<div class= "Answers">
+    <div><p>Answers des repenses fausses:</p></div>
+    <div><carte><p>${array[0]==undefined? "":array[0]}</p></carte></div>
+    <div><p>${array[1]==undefined? "":array[1]}</p></div>
+    <div><p>${array[2]==undefined? "":array[2]}</p></div>
+    <div><p>${array[3]==undefined? "":array[3]}</p></div>
+    <div><p>${array[4]==undefined? "":array[4]}</p></div>
+    <div><p>${array[5]==undefined? "":array[5]}</p></div>
+    <div><p>${array[6]==undefined? "":array[6]}</p></div>
+    <div><p>${array[7]==undefined? "":array[7]}</p></div>
+    <div><p>${array[8]==undefined? "":array[8]}</p></div>
+    <div><p>${array[9]==undefined? "":array[9]}</p></div>
+    <button id="previous" name="button" onclick=resultat() class="inactive">Resultat</button>    
 </div>
 `}
